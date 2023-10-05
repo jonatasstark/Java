50b707ca2869b6f46d20beaea7d7cef84b327896
jonatasstark <141359739+jonatasstark@users.noreply.github.com>
2023-10-05 16:30:45 -0300

    Update RaizQuadrada.java

--- RaizQuadrada.java
+++ RaizQuadrada.java
@@ -2,8 +2,8 @@ package Exercicios;
 import java.lang.Math;
 public class RaizQuadrada {
     public static void main(String[] args) {
-        float numero = 45;
-        double raiz_quadrada = Math.sqrt(numero);
+        float numero = 45;//declara um numero
+        double raiz_quadrada = Math.sqrt(numero);//declara a raiz de um numero
 
         System.out.printf("A raiz quadrada de %.1f é: %.2f", numero, raiz_quadrada);
     }
