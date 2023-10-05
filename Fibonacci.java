4304b20ce9f23131f3ad821eb9f6270bf2c4a81b
jonatasstark <141359739+jonatasstark@users.noreply.github.com>
2023-10-05 16:38:39 -0300

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -4,14 +4,14 @@ public class Fibonacci {
     public static void main(String[] args) {
         int num1 = 0;//declara um numero
         int num2 = 1;//declara outro numero
-        int num3;//
+        int num3;// declara uma variável para receber a sequência
 
         System.out.print(num1 + " ");
         System.out.print(num2 + " ");
-        for (int i = 3; i <= 10; i++){//loop for
-            num3 = num2 + num1;
-            num1 = num2;
-            num2 = num3;
+        for (int i = 3; i <= 10; i++){//loop for até 10
+            num3 = num2 + num1; // soma os numeros anteriores numero2 e numero1
+            num1 = num2; // numero1 passa a ser numero2
+            num2 = num3; // e numero2 passa a ser numero3 para fazer a sequência fibonacci
             System.out.print(num3 + " ");
         }
     }
