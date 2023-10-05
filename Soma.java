447f85cdd6904a88c349f03ce7ebd1c60397bb62
jonatasstark <141359739+jonatasstark@users.noreply.github.com>
2023-10-05 16:33:23 -0300

    Update Soma.java

--- Soma.java
+++ Soma.java
@@ -2,11 +2,11 @@ package Exercicios;
 
 public class Soma {
     public static void main(String[] args) {
-        int soma = 0;
-        int a = 1;
-        int b = 2;
+        int soma = 0;//declara uma variavel
+        int a = 1;//declara outro numero
+        int b = 2;//declara outro numero
 
-        soma = a + b;
+        soma = a + b;//faz a soma de a + b
 
         System.out.println("A SOMA DE " + a + " + " + b + " é: " + soma);
     }
