6161e17c50ed968133185028301e8035fb946f95
jonatasstark <141359739+jonatasstark@users.noreply.github.com>
2023-10-05 16:31:59 -0300

    Update Resto.java

--- Resto.java
+++ Resto.java
@@ -2,9 +2,9 @@ package Exercicios;
 
 public class Resto {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 3;
-        int resto = a % b;
+        int a = 10;//declara um numero
+        int b = 3;//declara outro numero
+        int resto = a % b;//recebe a % b
 
         System.out.println("O resto de " + a + " e " + b + " é: " + resto);
     }
