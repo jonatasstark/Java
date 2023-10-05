409cea451eea15da63ba67f9d70c164216e72773
jonatasstark <141359739+jonatasstark@users.noreply.github.com>
2023-10-05 16:29:33 -0300

    Update quociente.java

--- quociente.java
+++ quociente.java
@@ -2,9 +2,9 @@ package Exercicios;
 
 public class quociente {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 2;
-        int quociente = a / b;
+        int a = 10;//declara um numero
+        int b = 2;//declara outro numero
+        int quociente = a / b;//recebe a / b
         
         System.out.println("O quociente de " + a + " e " + b + " é: " + quociente );
 
