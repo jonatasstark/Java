7a952c8212fe4cedfeab8a0100e0145b9a3ba4ae
jonatasstark <141359739+jonatasstark@users.noreply.github.com>
2023-10-05 16:34:21 -0300

    Update TriploNumero.java

--- TriploNumero.java
+++ TriploNumero.java
@@ -2,8 +2,8 @@ package Exercicios;
 
 public class TriploNumero {
     public static void main(String[] args) {
-        int num1 = 20;
-        int triplo = num1 * 3;
+        int num1 = 20;//declara um numero
+        int triplo = num1 * 3;//calcula o triplo do numero
 
         System.out.println("O triplo de " + num1 + " é: " + triplo);
 
